dc2d82468504e970c777415d5c73dbffac27d4bf
ophir abitbol <ophir-abitbol@ophirs-iMac.local>
2015-08-17 16:36:49 +0300

    Initial Commit

---
+++ Machizmo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Machizmo
+//
+//  Created by ophir abitbol on 8/17/15.
+//  Copyright (c) 2015 ophir abitbol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Machizmo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Machizmo
+//
+//  Created by ophir abitbol on 8/17/15.
+//  Copyright (c) 2015 ophir abitbol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
