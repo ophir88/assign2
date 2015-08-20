253ec18c4e9e7f25df22b00c4a33b5e48a16f6db
ophir abitbol <ophir@lightricks.com>
2015-08-20 15:12:21 +0300

    temp

--- Machizmo/Model/SetCard.h
+++ Machizmo/Model/SetCard.h
@@ -7,7 +7,32 @@
 //
 
 #import "Card.h"
-
+#define COLOR 0
+#define SHAPE 1
+#define RANK 2
+#define HUE 3
+#define NUM_PROPERTIES 4
 @interface SetCard : Card
+
+
+// setCard properties which will define each card:
+@property (strong,nonatomic) NSString *shape;
+@property (strong,nonatomic) NSString *color;
+@property (strong,nonatomic) NSString *hue;
+@property (nonatomic) NSUInteger rank;
+
+
+// possible variations of the different properties:
++(NSArray *) validShapes;
++(NSArray *) validColors;
++(NSArray *) validHues;
++(NSArray *) validRanks;
+
+// This method checks wether two card match:
+// it returns an array containing YES/NO at each index
+// if the specific property matches.
+-(NSArray *) match:(Card *) card;
+
+
 
 @end
--- Machizmo/Model/SetCard.m
+++ Machizmo/Model/SetCard.m
@@ -10,4 +10,73 @@
 
 @implementation SetCard
 
+
+// valid properties of the cards:
+// -----------------------------
++(NSArray *) validShapes
+{
+    return @[@"▲",@"●",@"■"];
+}
+
++(NSArray *) validColors
+{
+    return @[@"red",@"green",@"purple"];
+}
++(NSArray *) validHues
+{
+    return @[@"soft",@"medium",@"strong"];
+}
++(NSArray *) validRanks
+{
+    return @[@"1",@"2",@"3"];
+}
+
+
+// Setters for the cards:
+// ----------------------
+-(void) setShape:(NSString *)shape
+{
+    // make sure it is a valid suit
+    if([ [SetCard validShapes] containsObject:shape] )
+    {
+        _shape = shape;
+    }
+}
+-(void) setColor:(NSString *)color
+{
+    // make sure it is a valid suit
+    if([ [SetCard validColors] containsObject:color] )
+    {
+        _color = color;
+    }
+}
+-(void) setHue:(NSString *)hue
+{
+    // make sure it is a valid suit
+    if([ [SetCard validHues] containsObject:hue] )
+    {
+        _hue = hue;
+    }
+}
+
+-(void) setRank:(NSUInteger ) rank
+{
+    if((rank <= [[SetCard validRanks] count ] -1) && rank >0)
+    {
+        _rank = rank;
+    }
+}
+
+
+
+
+-(NSArray *) match:(Card *) card;
+{
+    SetCard * otherCard = (SetCard *) card;
+    return @[@([self.color isEqualToString: otherCard.color]), @(self.shape==otherCard.shape), @(self.rank==otherCard.rank), @(self.hue==otherCard.hue)];
+}
+
+
+
+
 @end
