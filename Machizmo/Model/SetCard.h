b0d82b496d0be4b3bb3f7c6acd8d43e797c5510f
ophir abitbol <ophir@lightricks.com>
2015-08-20 15:09:09 +0300

    assignment 3 setCard

---
+++ Machizmo/Model/SetCard.h
@@ -0,0 +1,13 @@
+//
+//  SetCard.h
+//  Machizmo
+//
+//  Created by ophir abitbol on 8/20/15.
+//  Copyright (c) 2015 ophir abitbol. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+
+@end
---
+++ Machizmo/Model/SetCard.m
@@ -0,0 +1,13 @@
+//
+//  SetCard.m
+//  Machizmo
+//
+//  Created by ophir abitbol on 8/20/15.
+//  Copyright (c) 2015 ophir abitbol. All rights reserved.
+//
+
+#import "SetCard.h"
+
+@implementation SetCard
+
+@end
